c7fb6857923c35ba4fafe936a3529b54d982cc4d
Jacek Podkanski <ruby.object@googlemail.com>
2018-03-21 18:57:32 +0000

    factory bot config and user validation

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -1,6 +1,7 @@
 # spec/support/factory_bot.rb
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
+  config.before(:suite) { FactoryBot.reload }
 end
 
 # https://github.com/thoughtbot/factory_bot/blob/master/GETTING_STARTED.md#linting-factories
